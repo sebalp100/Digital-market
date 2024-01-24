254069f8a8fb9ee14a1705684dab2cc76e2132b8
sebalp100 <sebalp100@gmail.com>
2024-01-24 12:49:36 -0300

    Add form logic and validations

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 
 import { Icons } from '@/components/Icons';
@@ -7,8 +8,24 @@ import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from '@/lib/validators/account-credentials-validator';
 
 const page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
+
   return (
     <>
       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0"></div>
@@ -29,13 +46,14 @@ const page = () => {
         </div>
 
         <div className="grid gap-6">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid gap-2">
               <div className="grid gap-1 py-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
+                  {...register('email')}
                   className={cn({
-                    'focus-visible:ring-red-500': true,
+                    'focus-visible:ring-red-500': errors.email,
                   })}
                   placeholder="you@example.com"
                 />
@@ -43,8 +61,9 @@ const page = () => {
               <div className="grid gap-1 py-2">
                 <Label htmlFor="password">Password</Label>
                 <Input
+                  {...register('passsword')}
                   className={cn({
-                    'focus-visible:ring-red-500': true,
+                    'focus-visible:ring-red-500': errors.password,
                   })}
                   placeholder="Password"
                 />
