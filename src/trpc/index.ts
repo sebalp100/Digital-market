3a60584eb135b5d43d72ca7c61480bd9afad180a
sebalp100 <sebalp100@gmail.com>
2024-01-27 15:12:41 -0300

    Add users collection

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,8 @@
+import { authRouther } from './auth-router';
 import { publicProcedure, router } from './trpc';
 
 export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return 'hello';
-  }),
+  auth: authRouther,
 });
 
 export type AppRouter = typeof appRouter;
